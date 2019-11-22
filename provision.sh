d1fac90dd014a8d55761fc656984786624ded64b
Salva Cardona <salvacm99@hotmail.es>
2019-11-22 12:39:51 +0100

    subo todo

---
+++ provision.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install -y apache2 start
+sudo apt-get install mysql-server php5-mysql
+sudo mysql_install_db
+sudo apt-get install -y libapache2-mod-php5 php5 php5-mcrypt
